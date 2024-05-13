31927baaeefbd3b1ffd5e88a58433853a7fba66c
Vlad Kolhanov <vladi.kolganov@gmail.com>
2024-05-13 10:12:44 +0300

    install husky

--- frontend/src/Test.tsx
+++ frontend/src/Test.tsx
@@ -1,7 +1,7 @@
 import { wrapper } from '@/test.module.css'
 
 const Test = () => {
-	return <div className={wrapper}>Hello from Test</div>
+  return <div className={wrapper}>Hello from Test</div>
 }
 
 export default Test
